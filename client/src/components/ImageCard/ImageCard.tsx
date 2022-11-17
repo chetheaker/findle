1dc8819a7a68a1ea90f4e7f50c53af42162b6d27
chetheaker <che.theaker@gmail.com>
2022-11-17 17:57:42 +0100

    imageCard updated

--- client/src/components/ImageCard/ImageCard.tsx
+++ client/src/components/ImageCard/ImageCard.tsx
@@ -1,19 +1,21 @@
-// import moment from "moment";
 import './style.css';
-import LikeButton from '../LikeButton/LikeButton'
+import firebase from 'firebase/compat/app';
+import LikeButton from '../LikeButton/LikeButton';
 
-function ImageCard ({image, user}){
+type ImageCardProps = {
+  image: firebase.firestore.DocumentData;
+  user: any;
+};
 
- 
+function ImageCard({ image, user }: ImageCardProps) {
   return (
-    <div key={image.data.asset_id} className='movie_container'>
-        <h3 className='user'>{image.userName}  </h3>
-        <img className='movie_img' alt={image.usedPrompt} src={image.url} ></img>
-        <LikeButton image={image} user={user} ></LikeButton>
-        <h3 className="movie_headline">{image.usedPrompt}</h3>
+    <div key={image.data.asset_id} className="movie_container">
+      <h3 className="user">{image.userName} </h3>
+      <img className="movie_img" alt={image.usedPrompt} src={image.url}></img>
+      <LikeButton image={image} user={user}></LikeButton>
+      <h3 className="movie_headline">{image.usedPrompt}</h3>
     </div>
-  ) 
+  );
 }
-
 
 export default ImageCard;
