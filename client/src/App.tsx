ca0b7622fa2dc8f0567b20afa0e2b67b4a1e5945
chetheaker <che.theaker@gmail.com>
2022-11-18 11:56:32 +0100

    install: chakra-ui

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,11 @@
 import './App.css';
 import React from 'react';
 import firebase from 'firebase/compat/app';
-import { firestore } from './services/fireBaseInit';
 import { auth } from './services/fireBaseInit';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { useEffect, useState } from 'react';
 import { fetchImages, fetchContest } from './services/FireStore';
+import { ChakraProvider } from '@chakra-ui/react';
 
 // import components
 import Navbar from './components/Navbar/Navbar';
@@ -33,48 +33,51 @@ function App() {
   }, []);
 
   return (
-    <div className="App">
-      <Navbar/>
-      <SignOut auth={auth}/>
-      <h1 className="h1WC">
-        Trinity generates an image using AI based on  <br></br>
-        a specific prompt that includes 5 main keywords, then blurs it. <br></br>
-        You try to guess the prompt used, and difficulty is reduced on each try. <br></br>
-        Finally, you'll get the resulting image of the prompts you tried. <br></br>
-        Good luck! <br></br>
-      </h1>
-      <section className="section1">
-        {user ? (
-          <>
-            <CreateImage
-              setImages={setImages}
-              contests={contests}
-              user={user}
-            />
-            {contests.map((contest) => (
-              <ImageFeed key={contest.contestId} contest={contest}>
-                {images.map((image) => {
-                  if (image.contestIdRef === contest.contestId) {
-                    return (
-                      <ImageCard
-                        user={user}
-                        image={image}
-                        key={image.data.asset_id}
-                      />
-                    );
-                  }
-                  return <></>;
-                })}
-              </ImageFeed>
-            ))}
-          </>
-        ) : (
-          <SignIn auth={auth}/>
-        )}
-      </section>
-    </div>
+    <ChakraProvider>
+      <div className="App">
+        <Navbar />
+        <SignOut auth={auth} />
+        <h1 className="h1WC">
+          Trinity generates an image using AI based on <br></br>a specific
+          prompt that includes 5 main keywords, then blurs it. <br></br>
+          You try to guess the prompt used, and difficulty is reduced on each
+          try. <br></br>
+          Finally, you'll get the resulting image of the prompts you tried.{' '}
+          <br></br>
+          Good luck! <br></br>
+        </h1>
+        <section className="section1">
+          {user ? (
+            <>
+              <CreateImage
+                setImages={setImages}
+                contests={contests}
+                user={user}
+              />
+              {contests.map((contest) => (
+                <ImageFeed key={contest.contestId} contest={contest}>
+                  {images.map((image) => {
+                    if (image.contestIdRef === contest.contestId) {
+                      return (
+                        <ImageCard
+                          user={user}
+                          image={image}
+                          key={image.data.asset_id}
+                        />
+                      );
+                    }
+                    return <></>;
+                  })}
+                </ImageFeed>
+              ))}
+            </>
+          ) : (
+            <SignIn auth={auth} />
+          )}
+        </section>
+      </div>
+    </ChakraProvider>
   );
 }
-
 
 export default App;
