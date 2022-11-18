ba51733b3f7d65f12a7381c1cbe427ade3763cd8
chetheaker <che.theaker@gmail.com>
2022-11-18 12:44:17 +0100

    css refactor

--- client/src/components/ImageCard/ImageCard.tsx
+++ client/src/components/ImageCard/ImageCard.tsx
@@ -1,15 +1,15 @@
 import './ImageCard.css';
-import firebase from 'firebase/compat/app';
+// import firebase from 'firebase/compat/app';
 // import LikeButton from '../LikeButton/LikeButton';
 
 type ImageCardProps = {
-  image: firebase.firestore.DocumentData;
+  imageUrl: string;
 };
 
-function ImageCard({ image }: ImageCardProps) {
+function ImageCard({ imageUrl }: ImageCardProps) {
   return (
     <div className="image-container">
-      <img className="image" alt={image.usedPrompt} src={image.url}></img>
+      <img className="image" alt="to guess" src={imageUrl}></img>
       <h3 className="image-tag">Made by DALL-E, Open AI</h3>
     </div>
   );
