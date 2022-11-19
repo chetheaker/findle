05f12b00acccccf53906213a39743743193254ee
chetheaker <che.theaker@gmail.com>
2022-11-19 12:09:25 +0100

    hotfix

--- client/src/components/ImagesContainer/ImagesContainer.tsx
+++ client/src/components/ImagesContainer/ImagesContainer.tsx
@@ -20,21 +20,20 @@ function ImagesContainer({ images, guessCount }: ImagesContainerProps) {
     <div className="images-container">
       <div className="thumbnails">
         {images.map((image, index) => {
-          if (index < guessCount + 1) {
-            return (
-              <button className="thumbnail" onClick={() => handleClick(image)}>
-                <ImageCard imageUrl={image} key={index} />
-              </button>
-            );
-          } else {
-            return (
-              <div className="thumbnail">
-                <div className="image-container">
-                  <BiLockAlt color="white" size="4em" className="lock" />
-                </div>
-              </div>
-            );
-          }
+          return (
+            <button
+              className="thumbnail"
+              onClick={() => {
+                index < guessCount + 1 && handleClick(image);
+              }}
+              key={index}
+            >
+              <ImageCard imageUrl={image} />
+              {index < guessCount + 1 || (
+                <BiLockAlt color="white" size="2em" className="lock" />
+              )}
+            </button>
+          );
         })}
       </div>
       <div className="image-cont">
