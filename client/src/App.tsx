824da7e4d59cc337e663e35fe4319e5f19886f53
Adam Hallberg <adamdavidhallberg@gmail.com>
2022-11-21 13:39:29 +0100

    feat:add darkmode

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,5 @@
 import './App.css';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { auth } from './services/fireBaseInit';
 import { ChakraProvider } from '@chakra-ui/react';
@@ -10,11 +10,23 @@ import Navbar from './components/Navbar/Navbar';
 
 function App() {
   const [user] = useAuthState(auth as any);
+  const [darkmode, setDarkmode] = useState(false);
+  const root = document.getElementById('root') as HTMLElement
+  useEffect(()=> {
+    if (darkmode) {
+      root.style.setProperty("--bg-color", "#12171a");
+      root.style.setProperty("--secondary-color", "white");
+
+    } else {
+      root.style.setProperty("--bg-color", "white");
+      root.style.setProperty("--secondary-color", "black");
+    }
+  },[darkmode])
 
   return (
     <ChakraProvider>
       <div className="App">
-        <Navbar user={user} />
+        <Navbar user={user} setDarkmode={setDarkmode}/>
         <Contest />
       </div>
     </ChakraProvider>
--- client/src/components/Navbar/Navbar.tsx
+++ client/src/components/Navbar/Navbar.tsx
@@ -3,19 +3,21 @@ import HowToModal from '../HowToModal/HowToModal';
 import SignOut from '../SignOut/SignOut';
 import SignIn from '../SignIn/SignIn';
 import ProfileModal from '../ProfileModal/ProfileModal';
+import SettingsModal from '../SettingsModal/SettingsModal';
 
 type NavbarProps = {
+  setDarkmode: React.Dispatch<React.SetStateAction<boolean>>;
   user: any;
 };
 
-function Navbar({ user }: NavbarProps) {
+function Navbar({ user, setDarkmode }: NavbarProps) {
   return (
     <div className="navbar">
       <h1>TRINITY</h1>
       <div className='right'>
         <HowToModal />
         {user ? <ProfileModal /> : <SignIn />}
-        <div>⚙️</div>
+        <SettingsModal setDarkmode={setDarkmode} />
       </div>
     </div>
   );
