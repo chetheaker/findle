996d1fabb68ee822f4dcf2b93e44b3ebdc1c49c4
Adam Hallberg <adamdavidhallberg@gmail.com>
2022-11-21 20:23:03 +0100

    feat:added timer

--- client/src/components/Timer/Timer.tsx
+++ client/src/components/Timer/Timer.tsx
@@ -1,37 +1,41 @@
 import React from 'react';
-import { useState, useEffect } from 'react';
+import { useEffect } from 'react';
 
 type TimerProps = {
-  expirationDate: number;
+  creationDate: number;
 };
 
-const Timer = ({ expirationDate }: TimerProps) => {
-  const [remainingSeconds, setRemainingSeconds] = useState(0);
-
-  useEffect(() => {
-    const getTime = () => {
-      const time = expirationDate - Date.now();
-      setRemainingSeconds(Math.floor(time / 1000));
-    };
-    const interval = setInterval(() => getTime(), 1000);
-    return () => clearInterval(interval);
-  }, [remainingSeconds, expirationDate]);
-
-  const formatTime = () => {
-    const hours = Math.floor(remainingSeconds / (60 * 60)) % 24;
-    const mins = Math.floor(remainingSeconds / 60) % 60;
-    const seconds = Math.floor(remainingSeconds % 60);
-
-    if (hours === 0 && mins === 0) {
-      return `${seconds} seconds`;
-    } else if (hours === 0) {
-      return `${mins} minutes ${seconds}`;
-    } else {
-      return `${hours} hours ${mins} minutes ${seconds} seconds`;
-    }
-  };
-
-  return <div className="timer">{formatTime()}</div>;
+const Timer = ({ creationDate }: TimerProps) => {
+const endDate = new Date(creationDate + 86400).getTime();
+let hoursLeft = 0;
+let minutesLeft = 0
+let secondsLeft = 0
+ useEffect(() => {
+   setInterval(() => {
+   
+   const now = new Date().getTime();
+   
+   const remainingTime = endDate - now;
+   
+   const second = 1000;
+   
+   const minute = second * 60;
+   
+   const hour = minute * 60;
+   
+   
+   hoursLeft = Math.trunc(remainingTime / hour);
+   
+   minutesLeft = Math.trunc((remainingTime % hour) / minute);
+   
+   secondsLeft = Math.trunc((remainingTime % minute) / second);
+   
+   }, 1000);
+ })
+return (
+  <div>Timer:{hoursLeft} {minutesLeft} {secondsLeft}</div>
+)
 };
+
 
 export default Timer;
