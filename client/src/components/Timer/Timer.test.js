6992cf8381e0ef6e934ef90adb59cc4d0d5fe020
Adam Hallberg <adamdavidhallberg@gmail.com>
2022-11-22 11:55:12 +0100

    refactor:remove log

--- client/src/components/Timer/Timer.test.js
+++ client/src/components/Timer/Timer.test.js
@@ -4,6 +4,5 @@ import Timer from './Timer'
 test('renders Timer', () => {
   render(<Timer />);
   const element = screen.getByText('Next Contest in 0 hours 0 minutes 0 seconds')
-  console.log('log', element)
   expect(element).toBeInTheDocument();
 });
