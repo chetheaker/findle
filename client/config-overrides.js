c26cf9bb8db3c5dbbdaee3fc43190aa14f58d855
quantium <alexcollsoutumuro@gmail.com>
2022-11-23 11:34:56 +0100

    broke nft mint polyfill

--- client/config-overrides.js
+++ client/config-overrides.js
@@ -1,4 +1,5 @@
 const webpack = require('webpack');
+const NodePolyfillPlugin = require('node-polyfill-webpack-plugin');
 
 module.exports = function override(config) {
     const fallback = config.resolve.fallback || {};
@@ -9,14 +10,34 @@ module.exports = function override(config) {
         "http": require.resolve("stream-http"),
         "https": require.resolve("https-browserify"),
         "os": require.resolve("os-browserify"),
-        "url": require.resolve("url")
+        "url": require.resolve("url"),
     })
     config.resolve.fallback = fallback;
     config.plugins = (config.plugins || []).concat([
         new webpack.ProvidePlugin({
             process: 'process/browser',
-            Buffer: ['buffer', 'Buffer']
+            Buffer: ['buffer', 'Buffer'],
+            plugins: [new NodePolyfillPlugin()]
         })
     ])
+    
+    console.log('override')
+    let loaders = config.resolve
+    loaders.fallback = {
+        "fs": false,
+        "tls": false,
+        "net": false,
+        "http": require.resolve("stream-http"),
+        "https": false,
+        "http2": false,
+        "zlib": require.resolve("browserify-zlib") ,
+        "path": require.resolve("path-browserify"),
+        "stream": require.resolve("stream-browserify"),
+        "util": require.resolve("util/"),
+        "crypto": require.resolve("crypto-browserify"),
+        "process/browser": false,
+        "buffer": require.resolve("buffer")
+
+    }
     return config;
 }
