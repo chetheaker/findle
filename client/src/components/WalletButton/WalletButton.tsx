fb37e43dafe16b1b201a9cd87f0c017b4b287909
René Beltrán <renebeltranromero@gmail.com>
2022-11-24 12:50:45 +0100

    finished dev

--- client/src/components/WalletButton/WalletButton.tsx
+++ client/src/components/WalletButton/WalletButton.tsx
@@ -15,15 +15,19 @@ const WalletButton = () => {
   useEffect( () => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
     console.log(wallet)
-    const getBalance = async () => {
-      const userPubKey = wallet? wallet.publicKey! : undefined;
-      // eslint-disable-next-line react-hooks/exhaustive-deps
-      let balanceRes = userPubKey ? await connection.getBalance(userPubKey) : -1;
-      balanceRes = Math.floor((balanceRes/1000000000)*100)/100; // 1 billion lamports to 1 SOL (2 decimals)
-      setBalance(balanceRes)
-      // console.log('SOL', balance);
-    };
-    getBalance();
+    try {
+      const getBalance = async () => {
+        const userPubKey = wallet? wallet.publicKey! : undefined;
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+        let balanceRes = userPubKey ? await connection.getBalance(userPubKey) : -1;
+        balanceRes = Math.floor((balanceRes/1000000000)*100)/100; // 1 billion lamports to 1 SOL (2 decimals)
+        setBalance(balanceRes)
+        // console.log('SOL', balance);
+      };
+      getBalance();
+    } catch (error) {
+      console.log(error)
+    }
 
   }, [balance, connection, wallet]);
 
